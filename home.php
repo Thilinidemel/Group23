1649c342f8e584b36d6fb3a253c099004daead4e
Thilinidemel <84140286+Thilinidemel@users.noreply.github.com>
2022-01-26 22:57:05 +0530

    Home Page Header Update

--- home.php
+++ home.php
@@ -10,48 +10,49 @@ if (isset($_SESSION['ID']) && isset($_SESSION['User'])) {
   <head>
     <meta charset="utf-8">
     <title>HOME</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
     <link rel="stylesheet" href="style2.css"/>
   </head>
+
   <body>
     <div class="header">
 
       <div class="header_left">
-        <img src="i01.jpg" alt="shop">
-        <div class="searchbar">
-          <img src="search.png" alt="search" />
-          <input type="text" placeholder="Search fot items">
+        <img class="header_left_img" src="logo.jpg" alt="Shop"/>
+        <div class="header_input">
+          <span class="material-icons"> search </span>
+          <input type="text" placeholder="Search">
         </div>
       </div>
 
       <div class="header_middle">
-        <div class="header_middle_option active">
-          <img class="icons" src="home.png" alt="Home" />
+        <div class="header_option active">
+          <abbr title="Home"><span class="material-icons"> home </span></abbr>
         </div>
-        <div class="header_middle_option">
-          <img class="icons" src="female.png" alt="Friends" />
+        <div class="header_option">
+          <abbr title="Female Clothings"><span class="material-icons"> girl </span></abbr>
         </div>
-        <div class="header_middle_option">
-          <img class="icons" src="male.png" alt="Watch" />
+        <div class="header_option">
+          <abbr title="Male Clothings"><span class="material-icons"> boy </span></abbr>
         </div>
-        <div class="header_middle_option">
-          <img class="icons" src="kids.png" alt="Groups" />
+        <div class="header_option">
+          <abbr title="Kids Section"><span class="material-icons"> child_friendly </span></abbr>
         </div>
-        <div class="header_middle_option">
-          <img class="icons" src="cart.png" alt="Gaming" />
+        <div class="header_option">
+          <abbr title="Cart"><span class="material-icons"> shopping_cart </span></abbr>
         </div>
       </div>
 
       <div class="header_right">
-        <div class="header_right_left">
-          <div class="header_right_left_info">
-            <img class="profilepicture" src="girl.png" alt="Girl">
-            <h4><?php echo $_SESSION['User']; ?></h4>
-          </div>
+        <div class="header_info">
+          <img class="user_avatar" src="avatar2.png" alt="Hi,"/>
+          <h4><?php echo $_SESSION['User']; ?></h4>
         </div>
-        <img class="tools" src="menu.png" alt="Menu">
-        <img class="tools" src="messenger.png" alt="Messenger">
-        <img class="tools" src="notifications.png" alt="Notifications">
-        <img class="tools" src="account.png" alt="Account">
+        <abbr title="See Profile"><span class="material-icons"> add </span></abbr>
+        <abbr title="Comment"><span class="material-icons"> forum </span></abbr>
+        <abbr title="Notifications"><span class="material-icons"> notifications </span></abbr>
+        <abbr title="More"><span class="material-icons"> expand_more </span></abbr>
       </div>
     </div>
   </body>
