de9322aedda6ae3c4a6f11de95fe9130c810d24a
Thilinidemel <84140286+Thilinidemel@users.noreply.github.com>
2022-01-26 20:08:16 +0530

    Home page modify

--- home.php
+++ home.php
@@ -3,25 +3,57 @@ session_start();
 
 if (isset($_SESSION['ID']) && isset($_SESSION['User'])) {
 
-
-
-
-
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>HOME</title>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="style2.css"/>
   </head>
   <body>
-    <h1>Hello, <?php echo $_SESSION['User']; ?></h1><br>
-    <a href="logout.php">Logout</a>
+    <div class="header">
+
+      <div class="header_left">
+        <img src="i01.jpg" alt="shop">
+        <div class="searchbar">
+          <img src="search.png" alt="search" />
+          <input type="text" placeholder="Search fot items">
+        </div>
+      </div>
+
+      <div class="header_middle">
+        <div class="header_middle_option active">
+          <img class="icons" src="home.png" alt="Home" />
+        </div>
+        <div class="header_middle_option">
+          <img class="icons" src="female.png" alt="Friends" />
+        </div>
+        <div class="header_middle_option">
+          <img class="icons" src="male.png" alt="Watch" />
+        </div>
+        <div class="header_middle_option">
+          <img class="icons" src="kids.png" alt="Groups" />
+        </div>
+        <div class="header_middle_option">
+          <img class="icons" src="cart.png" alt="Gaming" />
+        </div>
+      </div>
+
+      <div class="header_right">
+        <div class="header_right_left">
+          <div class="header_right_left_info">
+            <img class="profilepicture" src="girl.png" alt="Girl">
+            <h4><?php echo $_SESSION['User']; ?></h4>
+          </div>
+        </div>
+        <img class="tools" src="menu.png" alt="Menu">
+        <img class="tools" src="messenger.png" alt="Messenger">
+        <img class="tools" src="notifications.png" alt="Notifications">
+        <img class="tools" src="account.png" alt="Account">
+      </div>
+    </div>
   </body>
 </html>
 
